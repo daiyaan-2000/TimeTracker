6af9bf54228cfcbd30019e98bb6b82cffbe02762
daiyaan-2000 <dcho0038@student.monash.edu>
2025-09-22 11:43:17 +0600

    Still inefficient

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -44,136 +44,7 @@ class HomePage extends StatelessWidget {
             //-------------------------------------------------------------------------------------
 
             //FIRST CONTAINER
-            Container(
-              margin: EdgeInsets.only(left: 100, right: 100, bottom: 50),
-              height: 150,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 255, 255, 255),
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    blurRadius: 8,
-                    spreadRadius: 1,
-                    offset: Offset(0, 4),
-                  ),
-                ],
-              ),
-              child: Padding(
-                padding: EdgeInsets.only(top: 20, right: 20, left: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            CircleAvatar(
-                              backgroundColor: Color.fromARGB(255, 27, 87, 110),
-                              radius: 25,
-                              child: Icon(Icons.computer, color: Colors.white),
-                            ),
-                            SizedBox(width: 20),
-                            Text(
-                              'Flutter UI Design',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 25,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Padding(
-                          padding: EdgeInsetsGeometry.only(
-                            left: 60,
-                            right: 20,
-                            bottom: 10,
-                            top: 1,
-                          ),
-                          child: Row(
-                            children: [
-                              //SizedBox(width: 100),
-                              Container(
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  color: Color.fromARGB(255, 95, 159, 184),
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                padding: EdgeInsets.only(
-                                  top: 5,
-                                  bottom: 5,
-                                  left: 10,
-                                  right: 10,
-                                ),
-                                child: (Text(
-                                  'Work',
-                                  style: TextStyle(
-                                    color: Color.fromARGB(255, 38, 88, 108),
-                                  ),
-                                )),
-
-                                //width: 70,
-                              ),
-                              SizedBox(width: 5),
-
-                              Container(
-                                padding: EdgeInsets.only(
-                                  top: 5,
-                                  bottom: 5,
-                                  left: 10,
-                                  right: 10,
-                                ),
-                                //width: 70,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  color: Color.fromARGB(255, 95, 159, 184),
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                child: (Text(
-                                  'Flutter Project',
-                                  style: TextStyle(
-                                    color: Color.fromARGB(255, 38, 88, 108),
-                                  ),
-                                )),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    Column(
-                      children: [
-                        RichText(
-                          text: TextSpan(
-                            children: [
-                              TextSpan(
-                                text: 'Time: ',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 17,
-                                ),
-                              ),
-                              TextSpan(
-                                text: '00:00:00',
-                                style: TextStyle(fontSize: 17),
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        Icon(Icons.play_arrow_rounded, size: 50),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ), // First child ends
+            timeWidget(), // First child ends
             //-------------------------------------------------------------------------------------
 
             //SECOND CONTAINER
@@ -743,6 +614,137 @@ class HomePage extends StatelessWidget {
           letterSpacing: 1.2,
           wordSpacing: 8,
         ),
+      ),
+    );
+  }
+}
+
+class timeWidget extends StatelessWidget {
+  const timeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 16),
+      height: 150,
+      width: double.infinity,
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 255, 255, 255),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 8,
+            spreadRadius: 1,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        spacing: 16,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Color.fromARGB(255, 27, 87, 110),
+                    radius: 25,
+                    child: Icon(Icons.computer, color: Colors.white),
+                  ),
+                  SizedBox(width: 20),
+                  Text(
+                    'Flutter UI Design',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsetsGeometry.only(
+                  left: 60,
+                  right: 20,
+                  bottom: 10,
+                  top: 1,
+                ),
+                child: Row(
+                  children: [
+                    //SizedBox(width: 100),
+                    Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 95, 159, 184),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding: EdgeInsets.only(
+                        top: 5,
+                        bottom: 5,
+                        left: 10,
+                        right: 10,
+                      ),
+                      child: (Text(
+                        'Work',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 38, 88, 108),
+                        ),
+                      )),
+
+                      //width: 70,
+                    ),
+                    SizedBox(width: 5),
+
+                    Container(
+                      padding: EdgeInsets.only(
+                        top: 5,
+                        bottom: 5,
+                        left: 10,
+                        right: 10,
+                      ),
+                      //width: 70,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 95, 159, 184),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: (Text(
+                        'Flutter Project',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 38, 88, 108),
+                        ),
+                      )),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          Column(
+            children: [
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Time: ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                      ),
+                    ),
+                    TextSpan(text: '00:00:00', style: TextStyle(fontSize: 17)),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Icon(Icons.play_arrow_rounded, size: 50),
+            ],
+          ),
+        ],
       ),
     );
   }
